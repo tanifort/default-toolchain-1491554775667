e4091cc1bdba769179f59432b05a63be7e1ae74f
tanifort <Ramond_Belmo.Tanifor_tamah@Student.Reutlingen-University.DE>
2017-04-14 23:36:27 +0200

    new5
    new5

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
-
-
+ 
 var express = require('express');
 var bodyParser = require('body-parser');
 var https = require('https');
@@ -9,59 +8,49 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
 var appEnv = cfenv.getAppEnv();
-//--------------------new ----------------configuration data i wrote -----------------
-//construntion.
+ 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+ 
 var Client = require("ibmiotf");
 var appClientConfig = {
     "org" : "2fbcv2",
-    "id" : "ray27",
+    "id" : "kati",
     "domain": "internetofthings.ibmcloud.com",
-    "auth-key" : "a-2fbcv2-opgagk3i5t",
-    "auth-token" : "OsfFOUQS+3Zk_a+vm7"
+    "auth-key" : "a-2fbcv2-ysbdesrq2j",
+    "auth-token" : "W+*cRFTjPdVp)-cOfB"
 }
-
+ 
 var appClient = new Client.IotfApplication(appClientConfig);
-//Add your code here
+ 
 appClient.connect();
-
+ 
 appClient.on("connect", function () {
-
-    appClient.subscribeToDeviceEvents("iot-phone","ray27","+","json");
-
+ 
+    appClient.subscribeToDeviceEvents("iot-phone","kati","+","json");
+ 
 });
-
 appClient.on("deviceEvent", function (deviceType, deviceId, eventType, format, payload) {
-                    var  pay= JSON.parse(payload);
-  
-	var x = pay.d.ax;
-	var y = pay.d.ay;
-	var z = pay.d.az;
-
-	if( (0.09 <=x || x<= -0.09 ) && (0.09 <=y || y<= -0.09)){
-        
-        console.log("Device Event from :: "+deviceType+" : "+deviceId+" of event "+eventType+" with payload : "+payload);
-      appClient.subscribeToDeviceStatus("iot-phone","ray27");
-
-     console.log(" your phone is motion");
-	}else{
-		console.log("your phone is on a plane surface");
+var obj = JSON.parse(payload);
+ 
+	var xAxis = obj.d.ax;
+	var yAxis = obj.d.ay;
+	var zAxis = obj.d.az;
+ 
+	if ((xAxis>0.03 || xAxis<-0.03) && (yAxis>0.03 || yAxis<-0.03)){
+		console.log("Device is moving");
 	}
-
-
-
-    
-
+	else{
+		console.log("Device is laying on table");
+	}
+  //  console.log("Device Event from :: "+deviceType+" : "+deviceId+" of event "+eventType+" with payload : "+payload);
+ 
 });
-
-
-
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
+ 
 var config = null;
 var credentials = null;
 if (process.env.VCAP_SERVICES) {
 	config = JSON.parse(process.env.VCAP_SERVICES);
-
+ 
 	var iotService = config['iotf-service'];
 	for (var index in iotService) {
 		if (iotService[index].name === 'discover-iot-try-service') {
@@ -71,13 +60,13 @@ if (process.env.VCAP_SERVICES) {
 } else {
 	console.log("ERROR: IoT Service was not bound!");
 }
-
+ 
 var basicConfig = {
 	org: credentials.org,
 	apiKey: credentials.apiKey,
 	apiToken: credentials.apiToken
 };
-
+ 
 var options = {
 	host: 'internetofthings.ibmcloud.com',
 	port: 443,
@@ -86,11 +75,11 @@ var options = {
 	},
 	auth: basicConfig.apiKey + ':' + basicConfig.apiToken
 };
-
+ 
 app.get('/credentials', function(req, res) {
 	res.json(basicConfig);
 });
-
+ 
 app.get('/iotServiceLink', function(req, res) {
 	var options = {
 		host: basicConfig.org + '.internetofthings.ibmcloud.com',
@@ -110,7 +99,7 @@ app.get('/iotServiceLink', function(req, res) {
 		org_res.on('end', function() {
 			try {
 				var org = JSON.parse(str);
-				var url = "https://console.ng.bluemix.net/#/resources/serviceGuid=" + org.bluemix.serviceInstanceGuid + "&orgGuid=" + org.bluemix.organizationGuid + "&spaceGuid=" + org.bluemix.spaceGuid;
+				var url = "https://c...content-available-to-author-only...x.net/#/resources/serviceGuid=" + org.bluemix.serviceInstanceGuid + "&orgGuid=" + org.bluemix.organizationGuid + "&spaceGuid=" + org.bluemix.spaceGuid;
 				res.json({ url: url });
 			} catch (e) { console.log("Something went wrong...", str); res.send(500); }
 			console.log("iotServiceLink end: ", str.toString());
@@ -118,14 +107,14 @@ app.get('/iotServiceLink', function(req, res) {
 	}).on('error', function(e) { console.log("ERROR", e); });
 	org_req.end();
 });
-
+ 
 app.post('/registerDevice', function(req, res) {
 	console.log(req.body);
 	var deviceId = null, typeId = "iot-phone", password = null;
 	if (req.body.deviceId) { deviceId = req.body.deviceId; }
 	if (req.body.typeId) { typeId = req.body.typeId; }
 	if (req.body.password) { password = req.body.password; }
-
+ 
 	var options = {
 		host: basicConfig.org + '.internetofthings.ibmcloud.com',
 		port: 443,
@@ -136,7 +125,7 @@ app.post('/registerDevice', function(req, res) {
 		method: 'POST',
 		path: 'api/v0002/device/types'
 	}
-
+ 
 	var deviceTypeDetails = {
 		id: typeId
 	}
@@ -148,7 +137,7 @@ app.post('/registerDevice', function(req, res) {
 		});
 		type_res.on('end', function() {
 			console.log("createDeviceType end: ", str.toString());
-
+ 
 			var dev_options = {
 				host: basicConfig.org + '.internetofthings.ibmcloud.com',
 				port: 443,
@@ -159,13 +148,13 @@ app.post('/registerDevice', function(req, res) {
 				method: 'POST',
 				path: 'api/v0002/device/types/'+typeId+'/devices'
 			}
-
+ 
 			var deviceDetails = {
 				deviceId: deviceId,
 				authToken: password
 			};
 			console.log(deviceDetails);
-
+ 
 			var dev_req = https.request(dev_options, function(dev_res) {
 				var str = '';
 				dev_res.on('data', function(chunk) {
@@ -183,16 +172,7 @@ app.post('/registerDevice', function(req, res) {
 	type_req.write(JSON.stringify(deviceTypeDetails));
 	type_req.end();
 });
-
+ 
 app.listen(appEnv.port, function() {
 	console.log("server starting on " + appEnv.url);
 });
-
-
-
-
-
-
-
-
-
